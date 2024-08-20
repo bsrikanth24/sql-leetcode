66a2fbb644e8cf9150fb26e8fbc485d0410ab59a
bsrikanth24 <118962183+bsrikanth24@users.noreply.github.com>
2024-08-20 11:58:28 +0530

    Update Exchange Seats.sql

--- Exchange Seats.sql/Exchange Seats.sql
+++ Exchange Seats.sql/Exchange Seats.sql
@@ -1,1 +1,34 @@
-aaa
+create table exchange_seats (
+id int,
+name varchar(20)
+);
+
+insert into exchange_seats(id,name) values(1, 'Doris'),(2, 'Abbot'),(3,'Green'),(4,'Emerson'),(5,'Jeames');
+
+select * from exchange_seats
+
+ID	NAME
+1	  Doris
+2	  Abbot
+3	  Green
+4	  Emerson
+5	  Jeames
+
+
+SELECT 
+    name,
+    CASE 
+        WHEN MOD(id, 2) = 0 THEN id - 1 
+        ELSE id + 1 
+    END AS id
+FROM 
+    exchange_seats
+ORDER BY 
+    id;
+
+NAME	  ID
+Abbot	   1
+Doris	   2
+Emerson	 3
+Green	   4
+Jeames	 5
