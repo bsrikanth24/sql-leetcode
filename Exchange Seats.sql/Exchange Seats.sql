89ea2481c9fe95d61f3640f18a10bd25e2ad659b
bsrikanth24 <118962183+bsrikanth24@users.noreply.github.com>
2024-08-20 12:20:34 +0530

    Update Exchange Seats.sql

--- Exchange Seats.sql/Exchange Seats.sql
+++ Exchange Seats.sql/Exchange Seats.sql
@@ -34,12 +34,23 @@ Green	   4
 Jeames	   5
 
 
-SELECT 
+/* SELECT 
     name,
     CASE 
         WHEN MOD(id, 2) = 0 THEN id - 1 
         when MOD(id, 2)!= 0 and id < count(*) over() then id + 1 else id end as id
   from exchange_seats
+
+NAME	ID
+A	    2
+B	    1
+C	    4
+D	    3
+E	    5  */
+
+select case when mod(id,2) = 0 then lag(name,1,name) over w else lead(name,1,name)
+over w end as name, id from test
+window w as (order by id) order by name;
 
 NAME	ID
 A	    2
